0c480e05dcc6a58614855024d5f246bc7f06b337
Somya Bhardwaj <Somyabhardwaj004@gmail.com>
2023-02-15 12:36:17 +0530

    my calculator

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
- // add event listener to form submit event
-      var form = document.getElementById("additionForm");
-      form.addEventListener("submit", function(event) {
-        event.preventDefault(); // prevent form submission
-        addNumbers();
-      });
+
 function addNumbers() {
  
     var num1 = document.getElementById("num1").value;
