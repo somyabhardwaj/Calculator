4d71c5ff47db59d7f8d8c0dbd8c345cf3c6dc3db
Somya Bhardwaj <Somyabhardwaj004@gmail.com>
2023-02-15 17:03:54 +0530

    Calculator

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-
-function addNumbers() {
+function open(){
+  document.querySelector("#h1")
+  h1.setAttribute('style', 'background-color:lightgray; color:red; font-style:italic; text-align: center; margin:0px');
+  }
+function Scan() {
  
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
-    var sum = parseFloat(num1) + parseFloat(num2);
-    document.getElementById("result").innerHTML = "The sum is: " + sum;
-   
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    
 }
+var num1,num2;
+function Sum(){
+  Scan();
+ 
+  var sum = parseFloat(num1) + parseFloat(num2);
+    document.getElementById("result").innerHTML = "The sum is: " + sum;
+}
+function Multiply(){
+  Scan();
+ 
+  var sum = parseFloat(num1) * parseFloat(num2);
+    document.getElementById("result").innerHTML = "The multiply is: " + sum;
+}
+function Substract(){
+  Scan();
   
+  var sum = parseFloat(num1) - parseFloat(num2);
+    document.getElementById("result").innerHTML = "The Substraction is: " + sum;
+}
+function Divide(){
+  Scan();
+  
+  var sum = parseFloat(num1) / parseFloat(num2);
+    document.getElementById("result").innerHTML = "The division is: " + sum;
+}
+
 
